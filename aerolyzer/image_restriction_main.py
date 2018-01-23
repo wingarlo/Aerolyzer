104031ec5386d4487223fd17f72790bbd4383157
wingarlo <wingarlo@oregonstate.edu>
2018-01-22 18:27:17 -0800

    edited yaml file to reflect the image restriction functions and vice versa

--- aerolyzer/image_restriction_main.py
+++ aerolyzer/image_restriction_main.py
@@ -31,13 +31,13 @@ def program(fxn, data, exifData, pathname):
         isVerified = {'meetsRest': False, 'error_message': "The image must be of a direct landscape with a sky and view"}
         return isVerified
     if not fxn.is_size(exifData['file size']):
-        isVerified = {'meetsRest': False, 'error_message': "The image must be no larger than 400kb"}
+        isVerified = {'meetsRest': False, 'error_message': "The image must be no larger than 4Mb"}
         return isVerified
     if not fxn.is_type(exifData['file type']):
         isVerified = {'meetsRest': False, 'error_message': "The file type of the image must be .jpg or .png"}
         return isVerified
     if not fxn.is_res(exifData['exif exifimagewidth'], exifData['exif exifimagelength']):
-        isVerified = {'meetsRest': False, 'error_message': "The image must be in the resolution range 1X1-1000X1000"}
+        isVerified = {'meetsRest': False, 'error_message': "The image must be in the resolution range 100X100-6000X6000"}
         return isVerified
     if not 'gps gpslatitude' in exifData or not 'gps gpslongitude' in exifData:
         isVerified = {'meetsRest': False, 'error_message': "Location services must be enabled for the camera"}
--- aerolyzer/readexif.py
+++ aerolyzer/readexif.py
@@ -1,12 +1,8 @@
 from retrieve_image_data import RtrvData as rd
 # Open image file for reading (binary mode)
 x = rd('./')
-tags = x.get_exif('./images/phones/i6s/IMG_0471.jpg',True,True)
+tags = x.get_exif('./images/img3.JPG',True,True)
 # Return Exif tags
+spec = tags['file size']
+print spec
 
-original = tags['exif datetimeoriginal']
-edited = tags['image datetime']
-if (original == edited):
-	return True
-else:
-	return False
