11093a894b36c3fa54f994ff74837d3d93127a9f
Logan Wingard <wingarlo@oregonstate.edu>
2018-02-19 12:55:59 -0800

    more testing increasing nn accuracy

--- aerolyzer/nn.py
+++ aerolyzer/nn.py
@@ -1,7 +1,5 @@
 import numpy as np
-def sigm(x,deriv=False):
-	if(deriv == True):
-		return x*(1-x)
+def sigm(x):
 	return 1 / (1 + np.exp(-x))
 def network(X,y):	
 	#X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
@@ -15,11 +13,17 @@ def network(X,y):
 		l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
 		syn1 += l1.T.dot(l2_delta)#synapse 1 (in between hidden layer and output)
 		syn0 += X.T.dot(l1_delta)#synapse 0 (in between input and hidden layer)	
-	print l2
+	#print l2
 	
 
 	print "testing with 1484949763_7_417.jpg [.3412,.4549,.5490,.0314,.1216,.3608] [1]"
 	X = np.array([.3412,.4549,.5490,.0314,.1216,.3608])
+	l1dup = sigm(np.dot(X,syn0))
+	l2dup = sigm(np.dot(l1dup,syn1))
+	print '%f' % (float(l2dup))
+
+	print "testing with 1484949787_52_1631.jpg [.7843,.4118,.3333,.8235,.4274,.3294] [0]"
+	X = np.array([.7843,.4118,.3333,.8235,.4274,.3294])
 	l1dup = sigm(np.dot(X,syn0))
 	l2dup = sigm(np.dot(l1dup,syn1))
 	print '%f' % (float(l2dup))
